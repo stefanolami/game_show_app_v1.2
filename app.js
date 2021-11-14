dbf5738f2e125c66497dcd2d8a9c2b81f1130d5e
stefanolami <stefanolami90@gmail.com>
2021-11-14 15:15:30 +0100

    finished with javascript file

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const qwerty = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase');
 const start = document.querySelector('.btn__reset');
-let missed = 0;
 
-start.addEventListener('click', () => {
-    const overlay = document.querySelector('#overlay');
-    overlay.style.display = 'none';
-})
+let missed = 0;
 
 const phrases = [
     'the science of today is the technology of tomorrow',
@@ -17,12 +13,12 @@ const phrases = [
 ];
 
 function getRandomPhraseAsArray(arr) {
-   const i = Math.floor(Math.random() * 4);
-   const newPhraseArray = arr[i].split('');
-   return newPhraseArray;
-}
+    const i = Math.floor(Math.random() * 4);
+    const newPhraseArray = arr[i].split('');
+    return newPhraseArray;
+ }
 
-function addPhraseToDisplay(arr) {
+ function addPhraseToDisplay(arr) {
     for (let i = 0; i < arr.length; i++) {
         const li = document.createElement('li');
         li.textContent = arr[i];
@@ -35,11 +31,9 @@ function addPhraseToDisplay(arr) {
     }
 }
 
-addPhraseToDisplay(getRandomPhraseAsArray(phrases));
-const letters = document.querySelectorAll('.letter');
-
 function checkLetter(btn) {
     let letter = '';
+    const letters = document.querySelectorAll('.letter');
     for (let i = 0; i < letters.length; i++) {
         if (letters[i].textContent === btn.textContent) {
             letters[i].className += ' show';
@@ -61,17 +55,68 @@ function checkWin() {
     if (letters.length === shownLetters.length) {
         overlay.className = 'win';
         overlay.style.display = 'flex';
+        createResetBtn();
     } else if (missed >= 5) {
         overlay.className = 'lose';
         overlay.style.display = 'flex';
+        createResetBtn();
     }
 }
+
+function createResetBtn() {
+    start.textContent = '';
+    const btn = document.createElement('button');
+    btn.textContent = 'Reset Game';
+    btn.className = 'resetBtn'
+    start.appendChild(btn);
+}
+
+function resetHearts() {
+    const lostHearts = document.querySelectorAll('.failedTry');
+    for (let i = 0; i < lostHearts.length; i++) {
+        lostHearts[i].className = 'tries';
+        lostHearts[i].firstChild.src = 'images/liveHeart.png';
+    }
+}
+
+function resetPhrase() {
+    phrase.innerHTML = '';
+    const newUl = document.createElement('ul');
+    phrase.append(newUl);
+    addPhraseToDisplay(getRandomPhraseAsArray(phrases));
+}
+
+function resetKeyboard() {
+    const usedKeys = document.querySelectorAll('.chosen');
+    for (let i = 0; i < usedKeys.length; i++) {
+        usedKeys[i].removeAttribute('class');
+        usedKeys[i].removeAttribute('disabled');
+    }
+}
+
+addPhraseToDisplay(getRandomPhraseAsArray(phrases));
+const letters = document.querySelectorAll('.letter');
+
+
+start.addEventListener('click', (e) => {
+    const overlay = document.querySelector('#overlay');
+    overlay.style.display = 'none';
+    
+    if (e.target.className === 'resetBtn') {
+        missed = 0;
+        resetHearts();
+        resetPhrase();
+        resetKeyboard();
+    }
+    
+})
+
 
 
 qwerty.addEventListener('click', (e) => {
     const btn = e.target
     if (btn.tagName === 'BUTTON') {
-        btn.className += ' chosen';
+        btn.className += 'chosen';
         btn.setAttribute('disabled', '');
         const letterFound = checkLetter(btn);
         if (letterFound === null) {
@@ -85,10 +130,3 @@ qwerty.addEventListener('click', (e) => {
     }
 })
 
-function createResetBtn() {
-    start.textContent = '';
-    const btn = document.createElement('button');
-    btn.textContent = 'Reset Game';
-    start.appendChild(btn);
-
-}
